c4aa38f1fcd1cb78e8a504284b283bbd96dd34ac
Jay-rO-o <jay-reply@outlook.kr>
2022-01-06 13:00:33 +0900

    Fix: git clone and sed
    - oh-my-zsh 설치후 터미널 exit 추가
    - git clone 안되던 문제 수정 (경로)
    - sed 명령어 대상 파일 누락 수정
    - README 수정

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
+# oh-my-zsh and vim vundle
 # Run
+> 1. Install Zsh 
+> ```bash
+> # Ubuntul
+> 
+> sudo apt install zsh
+> ```
+> 2. set default shell to zsh
+> ```bash
+> chsh -s `which zsh`
 ```bash
 $ ./bootstrap.sh
 ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+ZSH_CUSTOM_DIR=~/.oh-my-zsh/custom
 # - oh-my-zsh + powerlevel10k
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo exit | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # download syntax highlight
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM_DIR/plugins/zsh-syntax-highlighting
 # download auto suggestion
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM_DIR/plugins/zsh-autosuggestions
 # download powerlevel10k
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM_DIR/themes/powerlevel10k
 # setting plugins
 # plugins=(
 #	git
@@ -22,7 +22,7 @@ git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerl
 sed -i 's/plugins=(git)/plugins=(\n\tgit\n\tzsh-syntax-highlighting\n\tzsh-autosuggestions\n\tsudo\n\thistory\n\tcopydir\n\tcopyfile\n)/' ~/.zshrc
 # setting theme
 # ZSH_THEME="powerlevel10k/powerlevel10k"
-sed -i '/ZSH_THEME/ s/".*"/"powerlevel10k\/powerlevel10k"/'
+sed -i '/ZSH_THEME/ s/".*"/"powerlevel10k\/powerlevel10k"/' ~/.zshrc
 # apply .zshrc
 source ~/.zshrc
 
