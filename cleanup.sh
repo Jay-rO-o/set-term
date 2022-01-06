08c46e2d91d531fe8dcc4b58f30bbf6743828bc0
Jay-rO-o <jay-reply@outlook.kr>
2022-01-06 13:18:14 +0900

    Fix: 누락된항목 추가
    - p10k 및 zsh 관련 설정 삭제 추가

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,7 @@
 # uninstall .oh-my-zsh
 sudo chmod 755 ~/.oh-my-zsh/tools/uninstall.sh
 echo y | ~/.oh-my-zsh/tools/uninstall.sh
+rm -rf ~/.zsh* ~/.p10k*
 
 # remove vim vundle & vimrc
 rm -rf ~/.vim/ ~/.vimrc
